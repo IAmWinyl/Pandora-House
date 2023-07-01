3a6e802bcb1a1d9744f36b0f669f2443591abc83
Serra Abak <serra.abak@gmail.com>
2023-07-01 14:19:11 -0700

    Removed unnecessary CORS setup

--- src/main/java/com/serraabak/pandorahouse/PandoraHouseApplication.java
+++ src/main/java/com/serraabak/pandorahouse/PandoraHouseApplication.java
@@ -2,9 +2,6 @@ package com.serraabak.pandorahouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PandoraHouseApplication {
@@ -12,15 +9,5 @@ public class PandoraHouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PandoraHouseApplication.class, args);
 	}
-
-	// @Bean
-	// public WebMvcConfigurer corsConfigurer() {
-	// 	return new WebMvcConfigurer() {
-	// 		@Override
-	// 		public void addCorsMappings(CorsRegistry registry) {
-	// 			registry.addMapping("/api/item").allowedOrigins("http://localhost:9000");
-	// 		}
-	// 	};
-	// }
 
 }
